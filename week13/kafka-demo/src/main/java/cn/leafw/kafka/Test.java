6b6d5d43c651d55496075ec816b012083f8757ef
CareyWYR <wyr95626@163.com>
2022-07-30 19:34:25 +0800

    13-1

---
+++ week13/kafka-demo/src/main/java/cn/leafw/kafka/Test.java
@@ -0,0 +1,62 @@
+package cn.leafw.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * Test
+ *
+ * @author <a href="mailto:wyr95626@95626.cn">CareyWYR</a>
+ * @date 2022/7/30
+ */
+public class Test {
+    private static final String SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+    public static void main(String[] args) {
+        consume();
+        send();
+    }
+
+    private static void send() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", SERVERS);
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+        producer.send(new ProducerRecord<>("test", "aa", "bb"));
+    }
+
+    private static void consume() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", SERVERS);
+        properties.put("group.id", "test_group");
+        properties.put("enable.auto.commit", "false");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
+
